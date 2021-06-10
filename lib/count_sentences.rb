25d3f8c13ec002188d293e3d37b0fcc6d2380203
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-09 22:48:26 -0500

    self count sentences lab solution

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    final = []
+    self.split('!').each{ |array1|
+      array1.split('.').each{ |array2|
+        array2.split('?').each{ |array3|
+          final << array3
+        }
+      }
+    }
+    final.length
   end
+
 end
